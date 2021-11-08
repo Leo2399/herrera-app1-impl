3172e90ba005d44a4b4f5aa878f887a8ad0d9575
Leonardo Herrera <leo2399@gmail.com>
2021-11-07 21:45:08 -0500

    test cases

---
+++ src/test/java/baseline/WrapAndEditCellTest.java
@@ -0,0 +1,11 @@
+package baseline;
+
+import javafx.scene.text.Text;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WrapAndEditCellTest {
+
+}
